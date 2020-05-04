ebf46653faa504a67489f82d59748bc735c5448c
Junsung Park <zeipar@gmail.com>
2020-05-04 21:20:46 +0000

    Load data using AR's where method and iterate them. Also use of conditionals with given parameter, with nested .where

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,18 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+
+
+Store.create(name: "Surrey", annual_revenue: 224000, mens_apparel: false, womens_apparel: true)
+Store.create(name: "Whistler", annual_revenue: 1900000, mens_apparel: true, womens_apparel: false)
+Store.create(name: "Yaletown", annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
+
+@mens_store = Store.where(mens_apparel: true)
+@mens_store.each do | store |
+  puts "Store Name: #{store.name}, Annual Revenue: #{store.annual_revenue}"
+end
+
+@womens_store_lt_1M = Store.where(womens_apparel: true).where("annual_revenue < ?", [1000000])
+@womens_store_lt_1M.each do | store |
+  puts "Store Name: #{store.name}, Annual Revenue: #{store.annual_revenue}"
+end
