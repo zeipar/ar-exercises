a3f8432908b7fc9285a4f576ad418f3dc9c256fa
Junsung Park <zeipar@gmail.com>
2020-05-05 06:33:31 +0000

    Set one-to-many relationship between employees and stores. Added 6 employee data to employee table

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,13 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+
+# @store1 = Burnaby
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Jason", last_name: "Park", hourly_rate: 13)
+@store1.employees.create(first_name: "Pika", last_name: "Chu", hourly_rate: 1)
+
+# @store2 = Richmond
+@store2.employees.create(first_name: "Harry", last_name: "Potter", hourly_rate: 2)
+@store2.employees.create(first_name: "Ron", last_name: "Weasley", hourly_rate: 2)
+@store2.employees.create(first_name: "Hermione", last_name: "Granger", hourly_rate: 50)
