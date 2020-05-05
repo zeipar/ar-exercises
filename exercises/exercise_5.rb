814ec70faf8762c309cb7c6f47c246e67c424ccb
Junsung Park <zeipar@gmail.com>
2020-05-05 06:26:49 +0000

    use of .count / .size method to chain with .where

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,13 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+
+totalRevenue = Store.sum(:annual_revenue)
+
+avgRevenue = totalRevenue / Store.count
+
+puts "Total revenue for the entire company: #{totalRevenue}"
+puts "Average annual revenue for all stores: #{avgRevenue}"
+
+# .count and .size can both be used
+puts "No. stores generating $1M or more annually: #{ Store.where("annual_revenue >= ?", [1000000]).size}"
